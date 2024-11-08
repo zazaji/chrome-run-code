ddf9d9d17337cdb0e884f1cc087d5651a1c388bd
zazaji <zazaji@sina.com>
2024-11-09 06:55:59 +0800

    #7 by zazaji - commit

--- backend/main.py
+++ backend/main.py
@@ -12,22 +12,26 @@ import time
 from io import BytesIO
 from typing import List, Union
 import platform
-
-ALLOWED_TOKEN = "YourTokenHere"
-TIMEOUT_DURATION = 120  # 设置运行子进程超时时间，单位为秒
+from config import ALLOWED_TOKEN,TIMEOUT_DURATION
 
 app = FastAPI()
+
+
+
 save_path = "../run_code/"  # 修改为你的保存路径
-static_folder='../run_code/'
+# temp_folder是在save_path下一级的路径
+temp_folder='temp/'
 if not(os.path.exists(save_path)):
     os.mkdir(save_path)
-if not(os.path.exists(static_folder)):
-    os.mkdir(static_folder)
+if not(os.path.exists(save_path+temp_folder)):
+    os.mkdir(save_path+temp_folder)
 
-app.mount("/static", StaticFiles(directory=static_folder), name="static")
+app.mount("/static", StaticFiles(directory=save_path), name="static")
 
-languages={"python":"py","css":"css","javascript":"js","html":"html","markdown":"md","json":"json","yaml":"yml","text":"txt","bash":"sh","powershell":"ps1","sql":"sql","c":"c","cpp":"cpp","java":"java","kotlin":"kt","objective-c":"m","swift":"swift","typescript":"ts"}
 
+languages={"python":"py","css":"css","javascript":"js","html":"html","markdown":"md","json":"json","yaml":"yml","text":"txt","bash":"sh","sh":"sh","powershell":"ps1","sql":"sql","c":"c","cpp":"cpp","java":"java","kotlin":"kt","objective-c":"m","swift":"swift","typescript":"ts"}
+error_pyfilenames=['','pygame','os', 're', 'sys', 'time', 'subprocess', 'platform', 'os', 're', 'time', 'subprocess', 'platform', 'numpy', 'pandas', 'matplotlib', 'PIL', 'cv2', 'torch', 'tensorflow', 'keras', 'sklearn', 'scipy', 'seaborn', 'nltk', 'jieba', 'wordcloud', 'plotly', 'base64', 'io', 'json', 'yaml', 'csv', 'random', 'math', 'datetime', 'shutil', 'glob', 'argparse', 'logging', 'functools', 'itertools', 'collections', 'operator', 'statistics', 'string', 're', 'urllib', 'http', 'socket', 'ssl', 'hashlib']
+# print(languages.get("html".lower(), 'py'))
 
 # 添加 CORS 中间件
 app.add_middleware(
@@ -38,17 +42,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# 保存路径和支持语言的映射
+
 
 class CodeRequest(BaseModel):
     code: str
     run: bool
     isLocal:bool
-    language: str
+    language: str  # 添加语言字段
     token: str
 
 class Output(BaseModel):
     type: str
     data: Union[str, List[str]]  # 可以是字符串或字符串列表
+
+
+
+def remove_non_printable_chars(text: str) -> str:
+    # 替换不可见字符，但保留回车、汉字、空格等
+    rtext = re.sub(r'[^\x20-\x7E\x0A\x0D\u4e00-\u9fff]', '', text)
+    return rtext
+
 
 def get_run_command(source_filename):
     if platform.system() == 'Windows':
@@ -64,14 +78,6 @@ def get_run_command(source_filename):
         run_command = ['sh', source_filename]
     return run_command
 
-def remove_non_printable_chars(text: str) -> str:
-    # 替换不可见字符，但保留回车、汉字、空格等
-    rtext = re.sub(r'[^\x20-\x7E\x0A\x0D\u4e00-\u9fff]', '', text)
-    return rtext
-
-
-
-
 def save_file_with_directory(path, content):
     # 分离出目录和文件名
     directory, filename = os.path.split(path)
@@ -84,12 +90,85 @@ def save_file_with_directory(path, content):
     with open(os.path.join(directory, filename), 'w') as file:
         file.write(content)
 
+def get_filename_from_code(save_code,file_extension):
+    first_line = save_code.splitlines()[0]
+
+    if first_line.startswith("import "):
+        filename=temp_folder+str(int(time.time()))
+    else:
+        filename = re.sub(r'[^A-Za-z0-9_\\\-\/\.]+', '', first_line.split(' ')[-1].strip())[-100:]
+    if filename.endswith(file_extension):
+        filename = filename[:-len(file_extension)-1]
+
+    if filename.split("/")[-1] in error_pyfilenames:
+        filename=temp_folder+str(int(time.time()))
+    print(first_line,filename)
+    return filename
+
+def replace_show_plot(save_code):
+    # 针对 Python 和 Matplotlib 的特殊处理
+    code_with_saves = re.sub(
+        r'plt.show\(\)',
+        lambda match: 'plt.savefig(f"temp_{images_count}.png"); plt.clf(); print(f"[[[temp_{images_count}.png]]]");',
+        code_request.code
+    )
+    save_code= "images_count = 0\noutputs = []\n" + code_with_saves
+    # save_code =add_global_images_count(code_request.code)
+    return save_code
+
+def replace_base64_images(outputs):
+    # 检查占位符并替换为 Base64 图像
+    new_outputs = []
+    for output in outputs:
+        if isinstance(output.data, str):
+            if "[[[" in output.data:
+                # 如果是图片，将其转换为 Base64 编码并添加到 outputs
+                img_filename = save_path + output.data.replace("[[[", "").replace("]]]", "")
+                if os.path.exists(img_filename):
+                    # 将图像转换为 Base64 编码
+                    with open(img_filename, "rb") as img_file:
+                        base64_image = base64.b64encode(img_file.read()).decode('utf-8')
+                        image_output = f"data:image/png;base64,{base64_image}"
+                        output = Output(type="image", data=image_output)
+        new_outputs.append(output)
+    return new_outputs
+
+# def add_global_images_count(code):
+#     # 匹配包含 plt.show() 的函数
+#     def add_global_to_func(match):
+#         func_body = match.group(0)
+#         # 如果函数体中有 plt.show()，则在函数第一行插入 global images_count
+#         if 'plt.show()' in func_body:
+#             func_body_lines = func_body.split('\n')
+#             func_body_lines.insert(1, '    global images_count')  # 在第一行之后插入 global 声明
+#             return '\n'.join(func_body_lines)
+#         return func_body
+
+#     # 找出所有定义的函数，并处理函数体
+#     modified_code = re.sub(r'(def [\w_]+\([\w_, ]*\):\n(?:    .*\n?)*)', add_global_to_func, code)
+
+#     # 替换 plt.show() 为 plt.savefig() 逻辑
+#     code_with_saves = re.sub(
+#         r'plt.show\(\)',
+#         lambda match: (
+#             'plt.savefig(f"temp_{images_count}.png"); '
+#             'plt.clf(); '
+#             'print(f"[[[temp_{images_count}.png]]]"); '
+#             'images_count += 1'
+#         ),
+#         modified_code
+#     )
+
+#     save_code = "images_count = 0\noutputs = []\n" + code_with_saves
+#     return save_code
+
 @app.post("/runcode")  # 添加 token 依赖
 async def run_code(code_request: CodeRequest):
-    save_path = "../run_code/"  # 修改为你的保存路径
-
     if code_request.token != ALLOWED_TOKEN:
         return {"outputs": [{"type": "error", "data": "Invalid token"}]}
+
+    global save_path
+    global temp_folder
 
     outputs: List[Output] = []
     images_count = 0  # 计数生成的图像数量
@@ -98,29 +177,16 @@ async def run_code(code_request: CodeRequest):
     if 1:
     # try:
         # 提取第一行作为文件名
-        first_line = code_request.code.splitlines()[0].split(' ')[-1].strip()
-        filename = re.sub(r'[^A-Za-z0-9_\\\-\/\.]+', '', first_line)[-100:]
         file_extension = languages.get(code_request.language.lower(), 'py')
+        save_code = code_request.code
         print(file_extension)
-        if filename.endswith(file_extension):
-            filename = filename[:-len(file_extension)-1]
-        if filename.startswith('/') or filename.startswith('~/') :
-            save_path=""
-        if filename=="":filename="temp/"+str(int(time.time()))
+        filename=get_filename_from_code(save_code,file_extension)
+
         source_filename = f"{save_path}{filename}.{file_extension}"
         exec_filename = f"{save_path}{filename}.out"
-        save_code = code_request.code
 
-        if code_request.isLocal==False and code_request.language == "python" and "matplotlib.pyplot" in code_request.code:
-            # 针对 Python 和 Matplotlib 的特殊处理
-            code_with_saves = re.sub(
-                r'plt.show\(\)',
-                lambda match: 'plt.savefig(f"temp_{images_count}.png"); plt.clf(); print(f"[[[temp_{images_count}.png]]]");',
-                code_request.code
-            )
-            save_code = "images_count = 0\noutputs = []\n" + code_with_saves
-
-            # save_code =add_global_images_count(code_request.code)
+        if code_request.isLocal==False and code_request.language == "python" and "matplotlib.pyplot" in save_code:
+            save_code=replace_show_plot(save_code)
 
         if code_request.run == False:
             # 如果 run=False，只保存代码文件不执行、
@@ -140,14 +206,11 @@ async def run_code(code_request: CodeRequest):
                 else:
                     return {"outputs": [{"type": "text", "data": code_request.code.replace("<pre","<div").replace("</pre>","</div")}]}
             elif file_extension in ["python","py"]:
-                print('file_extension',file_extension)
-
                 result = subprocess.run(['python3', source_filename], capture_output=True, text=True,cwd=save_path, timeout=TIMEOUT_DURATION )
             elif file_extension in ["sh","bash"]:
-
                 if "rm -" in code_request.code:
-                    result_dict= {"returncode":0,"stdout":"Dangerous command detected.","stderr":"ERROR."}
-                    result=SimpleNamespace(**result_dict)
+                    result= {"returncode":0,"stdout":"Dangerous command detected.","stderr":"Dangerous command detected."}
+                    result=SimpleNamespace(**result)
                 else:
                     result = subprocess.run(get_run_command(source_filename), capture_output=True, text=True,cwd=save_path, timeout=TIMEOUT_DURATION )
             elif file_extension in ["javascript","js"]:
@@ -176,23 +239,8 @@ async def run_code(code_request: CodeRequest):
                 stdout_lines = result.stdout.strip().splitlines()
                 for line in stdout_lines:
                     outputs.append(Output(type="text", data=line))
+                new_outputs=replace_base64_images(outputs)
 
-                # 检查占位符并替换为 Base64 图像
-                new_outputs = []
-                for output in outputs:
-                    if isinstance(output.data, str):
-                        if "[[[" in output.data:
-                            # 如果是图片，将其转换为 Base64 编码并添加到 outputs
-                            img_filename = save_path + output.data.replace("[[[", "").replace("]]]", "")
-                            if os.path.exists(img_filename):
-                                # 将图像转换为 Base64 编码
-                                with open(img_filename, "rb") as img_file:
-                                    base64_image = base64.b64encode(img_file.read()).decode('utf-8')
-                                    image_output = f"data:image/png;base64,{base64_image}"
-                                    output = Output(type="image", data=image_output)
-                    new_outputs.append(output)
-
-                print(new_outputs)
                 return {"outputs": [output.dict() for output in new_outputs]}
             else:
                 # 如果执行失败，可以返回错误信息
